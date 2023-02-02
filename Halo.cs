15246b28f7f2a35cd70c468def9f3973364ba729
Nelsen Niko <123928431+nelsenpro@users.noreply.github.com>
2023-02-02 19:38:58 +0700

    Create Halo.cs

---
+++ Halo.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace HelloWorld
+
+{
+
+  class Program
+
+  {
+
+    static void Main(string[] args)
+
+    {
+
+      Console.WriteLine("Hello World!");    
+
+    }
+
+  }
+
+}
